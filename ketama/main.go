1a76bbabb3088b5c55b15b88583501a4673bb49a
chen <1735446621@qq.com>
2022-09-21 23:44:27 +0800

    方法定义

--- ketama/main.go
+++ ketama/main.go
@@ -1,1 +1,35 @@
 package ketama
+
+func init() {
+
+}
+
+// 哈希方法
+func hashFunc() {
+
+}
+
+// 获取key所在的物理节点信息
+func NodeLocation(key string) {
+
+}
+
+// 构建虚拟环，将物理节点映射为虚拟节点并落在环上
+func ringConstruct() {
+
+}
+
+// 新增物理节点
+func AddNode() {
+
+}
+
+// 移除物理节点
+func RemoveNode() {
+
+}
+
+// 数据rehash
+func dataMigrate() {
+
+}
